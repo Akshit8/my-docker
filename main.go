978c2e9da6eac7ba7de7582ea2293c847b3eaf06
Akshit Sadana <akshitsadana@gmail.com>
2021-02-10 16:39:03 +0530

    indentation change2

--- main.go
+++ main.go
@@ -27,16 +27,21 @@ func main() {
 func run() {
 	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
 	// proc dir is a directory where all processes metadata is there
+    // our temporary binary will also be present here
+    // below line executes child function inside the newly created container
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+    // attatching os-std process to our cmd-std process
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+    // setting some system process attributes
+    // below line of code is responsible for creating a new isolated process
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		// Cloning is what creates the process(container) in which we would be running our command.
-		// CLONE_NEWUTS will allow to have our own hostname inside our container.
+		// CLONE_NEWUTS will allow to have our own hostname inside our container by creating a new unix timesharing system.
 		// CLONE_NEWPID assigns pids to only process inside the new namspace.
-		// CLONE_NEWNS namspace for mount.
+		// CLONE_NEWNS new namespace for mount.
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 		// Mounts in systemd gets recursively shared property.
 		// Unshare the recursively shared property for new mount namespace.
@@ -44,6 +49,7 @@ func run() {
 		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
+    // running the command and catching error
 	if err := cmd.Run(); err != nil {
 		log.Fatal("Error: ", err)
 	}
